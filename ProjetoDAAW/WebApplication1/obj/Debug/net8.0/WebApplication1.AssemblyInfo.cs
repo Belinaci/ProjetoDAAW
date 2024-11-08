d666e61c570fb717d973285bff0507fcfb6348b8
Andre <andre.belinaci@hotmail.com>
2024-11-08 11:14:34 -0300

    Genero

--- ProjetoDAAW/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ ProjetoDAAW/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+836247564b1b893079a21ae5cc4815bd440a7329")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb10614424d089de6e511e32e617016bb0d50222")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
