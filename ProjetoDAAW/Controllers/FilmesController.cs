d7612209d032a1b026d7952b7034619fa7897cf9
mozerbtww <caraguta02@gmail.com>
2024-11-12 17:49:38 -0300

    Feito ligação de tabelas no controller e na view Index & Create

--- ProjetoDAAW/Controllers/FilmesController.cs
+++ ProjetoDAAW/Controllers/FilmesController.cs
@@ -22,6 +22,12 @@ namespace ProjetoDAAW.Controllers
         // GET: Filmes
         public async Task<IActionResult> Index()
         {
+            // Inclui os Gêneros e Filmes a serem mostrados no Index
+            var filmes = await _context.Filme
+                .Include(f => f.Genero)    
+                .Include(f => f.Artista)   
+                .ToListAsync();
+
             return View(await _context.Filme.ToListAsync());
         }
 
@@ -43,9 +49,14 @@ namespace ProjetoDAAW.Controllers
             return View(filme);
         }
 
+        /* ORIGINAL BACKUP
         // GET: Filmes/Create
         public IActionResult Create()
         {
+
+            // Traz a lista de Gêneros e Atores
+            ViewBag.Genero = new MultiSelectList(_context.Genero, "Id", "Nome");
+            ViewBag.Artista = new MultiSelectList(_context.Artista, "Id", "Nome");
             return View();
         }
 
@@ -58,12 +69,69 @@ namespace ProjetoDAAW.Controllers
         {
             if (ModelState.IsValid)
             {
+                // Adiciona os gêneros selecionados à lista de gêneros do filme
+                filme.Genero = _context.Genero.Where(g => generoSelecionados.Contains(g.Id)).ToList();
+
+                // Adiciona os artistas selecionados à lista de artistas do filme
+                filme.Artista = _context.Artista.Where(a => artistaSelecionados.Contains(a.Id)).ToList();
+
                 _context.Add(filme);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(filme);
         }
+        */
+
+        // GET: Filmes/Create
+        public IActionResult Create()
+        {
+            // Traz a lista de Gêneros e Artistas
+            ViewBag.Genero = new MultiSelectList(_context.Genero, "Id", "Nome");
+            ViewBag.Artista = new MultiSelectList(_context.Artista, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Filmes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Titulo,Descricao,DtLancamento,Diretor,FtCapaFilme")] Filme filme, int[] generoSelecionados, int[] artistaSelecionados)
+        {
+            // remove validação de Genero e Artista pq a gente esqueceu de adicionar o "?" na list<>
+            ModelState.Remove("Genero");
+            ModelState.Remove("Artista");
+
+            if (ModelState.IsValid)
+            {
+                // Adiciona os gêneros selecionados à lista de gêneros do filme
+                if (generoSelecionados != null && generoSelecionados.Length > 0)
+                {
+                    filme.Genero = _context.Genero
+                        .Where(g => generoSelecionados.Contains(g.Id))
+                        .ToList();
+                }
+
+                // Adiciona os artistas selecionados à lista de artistas do filme
+                if (artistaSelecionados != null && artistaSelecionados.Length > 0)
+                {
+                    filme.Artista = _context.Artista
+                        .Where(a => artistaSelecionados.Contains(a.Id))
+                        .ToList();
+                }
+
+                // Salva o filme no banco de dados
+                _context.Add(filme);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Se o modelo não for válido, repopula as listas de gêneros e artistas
+            ViewBag.Genero = new MultiSelectList(_context.Genero, "Id", "Nome");
+            ViewBag.Artista = new MultiSelectList(_context.Artista, "Id", "Nome");
+            return View(filme);
+        }
+
+
 
         // GET: Filmes/Edit/5
         public async Task<IActionResult> Edit(int? id)
